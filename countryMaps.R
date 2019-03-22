37f58b95e932cd41da206bf04be70293807b7a9f
Nicolas Kuzminski <43076549+nicokuz@users.noreply.github.com>
2019-03-22 12:08:24 -0300

    Removed

--- countryMaps.R
+++
@@ -1,3 +0,0 @@
-countryMaps <- geojsonio::geojson_read("http://github.com/simonepri/geo-maps/releases/download/v0.6.0/countries-land-10km.geo.json", what = "sp")
-
-saveRDS(countryMaps, "countryMaps.rds")
